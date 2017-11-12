ca77add11495657612db0e9fd81f2d14f2b38253
dane <dane_johnson@ymail.com>
2017-11-12 13:27:41 -0600

    init commit

---
+++ src/pokedb_frontend/core.cljs
@@ -0,0 +1,50 @@
+(ns pokedb-frontend.core
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+(enable-console-print!)
+
+(defonce app-state (atom {:text "Hello world!"}))
+(def num-pokes 50)
+
+(defn place-rand-poke
+  []
+  [(rand-int 400) (rand-int (.-innerWidth js/window)) (- (rand-int (.-innerHeight js/window)))])
+
+(def pokemons (atom (vec (repeatedly num-pokes place-rand-poke))))
+
+(defn update-poke
+  [poke]
+  (if (> (poke 2) (+ (.-innerHeight js/window) 100))
+    (place-rand-poke)
+    (update poke 2 (partial + 10))))
+
+(defn update-pokes!
+  []
+  (swap! pokemons #(map update-poke %))) 
+
+(defn pokemon-img [number x y]
+  [:div {:style {:width 100 :height 100 :overflow "hidden"
+                 :position "absolute" :top y :left x}}
+   [:img {:src "/img/pokesprites.png"
+          :style {:margin-left (* -96 (mod number 31))
+                  :margin-top  (* -96 (quot number 31))}}]])
+
+(defn poke-div
+  []
+  [:div {:style {:position "absolute" :top 0 :left 0}}
+   (map-indexed #(with-meta (apply pokemon-img %2) {:key %1}) @pokemons)])
+
+(defn hello-world []
+  [:div
+   [poke-div]])
+
+(defn animate
+  []
+  (do
+    (update-pokes!)
+    (js/setTimeout animate 50)))
+
+(js/setTimeout animate 50)
+
+(reagent/render-component [hello-world]
+                          (. js/document (getElementById "app")))
