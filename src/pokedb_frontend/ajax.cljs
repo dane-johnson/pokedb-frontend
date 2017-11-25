51c136030bda19aa79a99fb7363966ecc11882eb
dane <dane_johnson@ymail.com>
2017-11-25 10:15:46 -0600

    Can update ages

--- src/pokedb_frontend/ajax.cljs
+++ src/pokedb_frontend/ajax.cljs
@@ -4,11 +4,13 @@
 
 (def ^:dynamic *api-url* "http://localhost:5000")
 
-(def trainers (atom (list)))
+(def trainers (atom {}))
 
 (defn trainers-handler
   [res]
-  (reset! trainers (cljs.reader/read-string res)))
+  (reset! trainers
+          (let [ts (cljs.reader/read-string res)]
+            (zipmap (map #(get % :number) ts) ts))))
 
 (defn trainers-error-handler
   [err]
@@ -19,3 +21,9 @@
   (GET (str *api-url*  "/trainers")
        {:handler trainers-handler
         :error-handler trainers-error-handler}))
+
+(defn set-age
+  [no age]
+  (PUT (str *api-url* "/trainer/" no)
+       {:params {:age age}
+        :format :raw}))
--- src/pokedb_frontend/core.cljs
+++ src/pokedb_frontend/core.cljs
@@ -1,13 +1,31 @@
 (ns pokedb-frontend.core
   (:require [reagent.core :as reagent :refer [atom]]
-            [pokedb-frontend.ajax :refer [trainers get-trainers]]
+            [pokedb-frontend.ajax :refer [trainers get-trainers set-age]]
             [pokedb-frontend.background :refer [scrolling-pokes]]))
 
 (enable-console-print!)
 (get-trainers)
 
+(defn on-age-change
+  [e]
+  (let [age (cljs.reader/read-string (-> e .-target .-value))
+        number (cljs.reader/read-string (-> e .-target .-name))]
+    (do
+      (set-age number age)
+      (swap! trainers assoc-in [number :age]  age))))
+
+(defn trainers-div
+  [[number t]]
+  (with-meta [:p (:name t) [:input {:type "number"
+                                    :min 1
+                                    :max 255
+                                    :value (:age t)
+                                    :name number
+                                    :on-change on-age-change}]]
+    {:key number}))
+
 (defn trainers-view []
-  [:div (map #(with-meta [:p (:name %)] {:key (:number %)}) @trainers)])
+  [:div (map trainers-div @trainers)])
 
 (defn page []
   [:div [scrolling-pokes] [trainers-view]])
